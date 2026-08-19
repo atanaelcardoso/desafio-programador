682b0f39be309f556cd20d056d9f08b8ce616d90
Atanael Cardoso <110429151+atanaelcardoso@users.noreply.github.com>
2026-08-19 17:46:26 -0300

    feat: alteracao url frontend

--- frontend/src/infra/api/api.js
+++ frontend/src/infra/api/api.js
@@ -2,23 +2,23 @@ import axios from 'axios'
 
 export const api = {
     getById: async (id) => {
-        const response = await axios.get(`/api/transcricoes/${id}`)
+        const response = await axios.get(`${process.env.API_URL}/api/transcricoes/${id}`)
         return response.data
     },
 
     update: async (id, value) => {
-        const response = await axios.put(`/api/transcricoes/${id}`, { value })
+        const response = await axios.put(`${process.env.API_URL}/api/transcricoes/${id}`, { value })
         return response.data
     },
 
     getDownloadFile: async (id, format) => {
-        return await axios.get(`/api/transcricoes/${id}/planilha`, {
+        return await axios.get(`${process.env.API_URL}/api/transcricoes/${id}/planilha`, {
             params: { formato: format },
             responseType: format === 'json' ? 'json' : 'blob'
         })
     },
      uploadDocument: async (formData) => {
-        const response = await axios.post('/api/transcricoes', formData, {
+        const response = await axios.post(`${process.env.API_URL}/api/transcricoes`, formData, {
             headers: { 'Content-Type': 'multipart/form-data' }
         })
         return response.data
